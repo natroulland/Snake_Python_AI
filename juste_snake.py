e17f10cd30929aa40dc8dbdd8633c7d3d1cd115a
natroulland <nathan.roulland@viacesi.fr>
2023-12-09 19:20:27 +0100

    Modifications pour passer sur une grille 10x10

--- juste_snake.py
+++ juste_snake.py
@@ -17,17 +17,19 @@ Point = namedtuple('Point', 'x, y')
 
 # rgb colors
 WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
 RED = (200,0,0)
 BLUE1 = (0, 0, 255)
 BLUE2 = (0, 100, 255)
-BLACK = (0,0,0)
+GREEN1 = (153, 255, 51)
+GREEN2 = (178, 255, 102)
 
-BLOCK_SIZE = 20
-SPEED = 20
+BLOCK_SIZE = 64
+SPEED = 10
 
 class SnakeGame:
     
-    def __init__(self, w=640, h=480):
+    def __init__(self, w=640, h=640):
         self.w = w
         self.h = h
         # init display
@@ -40,8 +42,8 @@ class SnakeGame:
         
         self.head = Point(self.w/2, self.h/2)
         self.snake = [self.head, 
-                      Point(self.head.x-BLOCK_SIZE, self.head.y),
-                      Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
+                      Point(self.head.x, self.head.y-BLOCK_SIZE),
+                      Point(self.head.x, self.head.y-(2*BLOCK_SIZE))]
         
         self.score = 0
         self.food = None
@@ -104,15 +106,21 @@ class SnakeGame:
         return False
         
     def _update_ui(self):
-        self.display.fill(BLACK)
+        for y in range(0, self.h, BLOCK_SIZE):
+            for x in range(0, self.w, BLOCK_SIZE):
+                # Alternance des couleurs
+                if (x + y) // BLOCK_SIZE % 2 == 0:
+                    pygame.draw.rect(self.display, GREEN1, (x, y, BLOCK_SIZE, BLOCK_SIZE))
+                else:
+                    pygame.draw.rect(self.display, GREEN2, (x, y, BLOCK_SIZE, BLOCK_SIZE))
         
         for pt in self.snake:
             pygame.draw.rect(self.display, BLUE1, pygame.Rect(pt.x, pt.y, BLOCK_SIZE, BLOCK_SIZE))
-            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+4, pt.y+4, 12, 12))
+            pygame.draw.rect(self.display, BLUE2, pygame.Rect(pt.x+12.8, pt.y+12.8, 38.4, 38.4))
             
         pygame.draw.rect(self.display, RED, pygame.Rect(self.food.x, self.food.y, BLOCK_SIZE, BLOCK_SIZE))
         
-        text = font.render("Score: " + str(self.score), True, WHITE)
+        text = font.render("Score: " + str(self.score), True, BLACK)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
         
@@ -132,16 +140,17 @@ class SnakeGame:
             
 
 if __name__ == '__main__':
-    game = SnakeGame()
-    
-    # game loop
-    while True:
-        game_over, score = game.play_step()
+    for i in range(10):
+        game = SnakeGame()
         
-        if game_over == True:
-            break
-        
-    print('Final Score', score)
-        
+        # game loop
+        while True:
+            game_over, score = game.play_step()
+            
+            if game_over == True:
+                break
+            
+        print('Final Score', score)
+            
         
     pygame.quit()
