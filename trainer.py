2a0850a153a8352bd261e08709d71e1933840e96
Nathan Roulland <nathan.roulland@viacesi.fr>
2024-01-04 14:43:00 +0100

    Ajout des meilleurs scores et fitness sur une génération

--- trainer.py
+++ trainer.py
@@ -5,25 +5,40 @@ from snake import Snake
 class SnakeTrainer:
     def __init__(self):
         self.snakes = [Snake(ADN()) for i in range(100)]
+        self.bestFitness = 0
+        self.bestScore = 0
+        self.bestSnake = self.snakes[0]
+        self.bestGenFitness = 0
+        self.generation = 0
+        self.totalGenScore = 0
+        self.bestGenScore = 0
     
     def playing(self):
-        game = snake_random.SnakeGame()
-        # game loop
-        while True:
-            vision = game.vision()
-            j = self.feur.choose_move(vision)
-            game_over, score = game.play_step(j)
-            game.steps += 1
-            game.total_steps += 1
-            if game_over == True:
-                game.steps = 0
-                break
-            
-        print('-------------------------------------')
-        print('Final Score', score)
-        print('Total Steps', game.total_steps)
-        fitness = (score*score) * (1/(game.total_steps))
-        print('Fitness = ', fitness) # Score de fitness pour comparer les individus
+        for snake in self.snakes:
+            game = snake_random.SnakeGame()
+            # game loop
+            while True:
+                vision = game.vision()
+                j = snake.choose_move(vision)
+                game_over, score = game.play_step(j)
+                game.steps += 1
+                game.total_steps += 1
+                if game_over == True:
+                    game.steps = 0
+                    break
+            fitness = (score*score) * (1/(game.total_steps))
+            if fitness > self.bestFitness:
+                self.bestFitness = fitness
+                self.bestSnake = snake
+            if score > self.bestScore:
+                self.bestScore = score
+            self.totalGenScore += score
+        print("Generation : ", self.generation)
+        print("Best fitness : ", self.bestFitness)
+        print("Best score : ", self.bestScore)
+        print("Average score : ", self.totalGenScore/100)
+        print("Total Score : ", self.totalGenScore)
+
     
 if __name__ == "__main__":
     trainer = SnakeTrainer()
